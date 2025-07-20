3f8ab42dd1b0a10f385f560ddcde9c2bfa493db5
Ian Rasbheri <ian@irasbheri.lan>
2025-07-20 21:12:30 +0100

    Add push to GitHub

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,6 +2,13 @@
 
 MODULE=$(basename $PWD)
 
+# Git check must pass before this is run
+git-check
+if [ $? -ne 0 ]; then
+  echo -e "\nERROR: git-check must pass BEFORE this can be run!"
+  exit 1
+fi
+
 # MkDocs for the MODULE
 echo "Create mkdocs.yaml for ${MODULE} ..."
 uthisha-export --module "${MODULE}" --format "mkdocs" > mkdocs.yml
@@ -12,3 +19,17 @@ for i in {1..4}; do
   echo "uthisha-export --module ${MODULE} --format day-md --day-code D${i}"
         uthisha-export --module ${MODULE} --format day-md --day-code D${i} > "docs/tne/day${i}.md"
 done
+
+# Run git check again to see if files were changed
+git-check
+if [ $? -eq 0 ]; then
+  echo -e "\nINFO: No files were changed."
+  exit 0
+fi
+
+# Commit and push changes to GitHub
+git add .
+git commit -m "Updated content .md files"
+git push
+
+#EOF
