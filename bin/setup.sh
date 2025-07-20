93b7aa60bb04cfee7eaaee3ef1905159e9c7bb94
Ian Rasbheri <ian@irasbheri.lan>
2025-07-20 20:36:35 +0100

    not 5 days

--- bin/setup.sh
+++ bin/setup.sh
@@ -8,7 +8,7 @@ uthisha-export --module "${MODULE}" --format "mkdocs" > mkdocs.yml
 
 # Agenda for the DAY
 echo -e "\nCreate daily agenda for  ${MODULE}:\n"
-for i in {1..5}; do
+for i in {1..4}; do
   echo "uthisha-export --module ${MODULE} --format day-md --day-code D${i}"
         uthisha-export --module ${MODULE} --format day-md --day-code D${i} > "docs/tne/day${i}.md"
 done
